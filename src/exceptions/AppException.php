af355759e2f1162a1b895c54c2195b0067d3e0fd
Ditt GitHub-användarnamn <din.email@exempel.com>
2025-11-20 18:48:13 +0100

    added error in AppExcepyion

--- src/exceptions/AppException.php
+++ src/exceptions/AppException.php
@@ -14,6 +14,7 @@ namespace Najla\Exceptions;
  */
 class AppException extends \Exception
 {
+    private $error;
 
     /**
      * Construct the exception
@@ -23,10 +24,25 @@ class AppException extends \Exception
      * 
      * @param string $message The message
      * @param int $code The code
+     * @param string $error The error
      * @return void
      */
-    public function __construct(string $message, int $code = 400)
+    public function __construct(string $message, int $code = 400, string $error = '')
     {
         parent::__construct($message, $code);
+        $this->error = $error;
+    }
+
+    /**
+     * Get the error
+     * 
+     * This method:
+     * - Returns the error
+     * 
+     * @return string The error
+     */
+    public function getError(): string
+    {
+        return $this->error;
     }
 }
